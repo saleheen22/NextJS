15366bfe060c5ea3bf7fa38e8116dbd08bd6b48f
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-05-28 13:22:08 -0500

    Add loading and error components for improved user experience

---
+++ app/(root)/about/loading.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">--------------------</h1>
+        </div>
+    );
+};
+
+export default loading;
---
+++ app/(root)/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from "react";
+
+export default function Error() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-600">Something went wrong!</h1>
+    </div>
+  );
+}
